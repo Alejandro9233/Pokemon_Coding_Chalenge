bdabf4598e70ac4c019da30ca215c425a01ab39c
Alejandro Barrera <114511645+Alejandro9233@users.noreply.github.com>
2023-09-29 02:01:10 +0000

    filtyer type

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -5,15 +5,12 @@ import Filter from "./Filter";
 import SidebarComponent from "./SideBar";
 import { List, Card } from "antd";
 import { Link } from "react-router-dom";
-
 const PokemonList = () => {
   const [pokemon, setPokemon] = useState([]);
   const [filter, setFilter] = useState("");
   const [sort, setSort] = useState("id");
   const [filterID, setPokemonID] = useState("");
-  const [filterRangeStart, setPokemonRangeStart] = useState("")
-  const [filterRangeEnd, setPokemonRangeEnd] = useState("")
-  
+  const [type, setType] = useState("");
 
   useEffect(() => {
     fetchData("/pokemon")
@@ -21,7 +18,29 @@ const PokemonList = () => {
         setPokemon(data.results);
       })
       .catch((error) => console.error(error));
-  }, []);
+  }, [type]);
+
+  useEffect(() => {
+    // Map over the Pokemon array and make a fetch request for each Pokemon
+    const pokemonPromises = pokemon.map(pokemon => {
+      return fetch(pokemon.url)
+        .then(response => response.json());
+    });
+  
+    // Wait for all fetch requests to complete and update the Pokemon list
+    Promise.all(pokemonPromises)
+      .then(pokemonList => {
+        if (type !== "") {
+          // Filter the Pokemon list based on the selected type
+          const filteredPokemon = pokemonList.filter(p => p.types.some(t => t.type.name === type));
+          setPokemon(filteredPokemon);
+        } else {
+          // If no type is selected, set the Pokemon list to all fetched Pokemon
+          setPokemon(pokemonList);
+        }
+      });
+  }, [type, pokemon]);
+
 
   const handleFilterChange = (filter) => {
     setFilter(filter);
@@ -32,34 +51,48 @@ const PokemonList = () => {
   };
 
   const handleFilterIDChange = (filterID) => {
-    setPokemonID(filterID)
+    setPokemonID(filterID);
   };
 
-  const handleFilterRangeStartChange = (filterRangeStart) => {
-    setPokemonRangeEnd(filterRangeStart)
-  };
-
-  const handleFilterRangeEndtChange = (filterRangeEnd) => {
-    setPokemonID(filterRangeEnd)
+  const handleTypeChange = (value) => {
+    setType(value);
   };
 
   const getImageUrl = (url) => {
+    if (!url) {
+      console.error('URL is undefined');
+      return;
+    }
+  
     const imageUrl =
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
     const pokemonUrl = "https://pokeapi.co/api/v2/pokemon/";
-
+  
     return imageUrl + url.replace(pokemonUrl, "").replace("/", ".png");
   };
+  
 
-  const filteredPokemonName = useMemo(() => filterDataByName(pokemon, filter), [filter,pokemon]);
-  const sortedPokemon = useMemo(() => sortData(filteredPokemonName, sort), [sort, filteredPokemonName]);
-  const filteredPokemonID = useMemo(() => filterDataByIndex(sortedPokemon, filterID), [filterID, sortedPokemon]);
-  console.log(filteredPokemonID);
+  const filteredPokemonName = useMemo(() => filterDataByName(pokemon, filter), [
+    filter,
+    pokemon,
+  ]);
+  const sortedPokemon = useMemo(() => sortData(filteredPokemonName, sort), [
+    sort,
+    filteredPokemonName,
+  ]);
+  const filteredPokemonID = useMemo(
+    () => filterDataByIndex(sortedPokemon, filterID),
+    [filterID, sortedPokemon]
+  );
+
+console.log(filteredPokemonID);
+console.log(type);
+
   return (
     <div style={{ width: "1200px", margin: "0 auto", padding: "20px", display: "flex", background: "transparent"  }}>
         <SidebarComponent
           onFilterChange={handleFilterIDChange}
-          onSortChange={handleSortChange}
+          onTypeChange={handleTypeChange}
         />
       <div style={{ width: '100%' }}>
         <Filter
@@ -81,7 +114,7 @@ const PokemonList = () => {
                   hoverable
                   style={{ borderRadius: "10px", transition: "all 0.3s ease", 
                   background: "-webkit-linear-gradient(45deg, rgba(213, 15, 61, 0.6), rgba(13, 17, 198, 0.69) 100%)" }}
-                  cover={<img alt={poke.name} src={getImageUrl(poke.url)} />}
+                  cover={<img alt={poke.name} src={poke.sprites?.front_default} />}
                   bodyStyle={{ display: "flex" }}
                 >
                   <Card.Meta title={poke.name} style={{ color: '#fff'}} />
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -4,14 +4,19 @@ import { Input, Select, Layout } from 'antd';
 const { Option } = Select;
 const { Sider } = Layout;
 
-const SidebarComponent = ({ onFilterChange, onSortChange }) => {
+const SidebarComponent = ({ onFilterChange, onTypeChange }) => {
   const [filterValue, setFilterValue] = useState('');
+
   
   const handleFilterChange = (event) => {
     const value = event.target.value;
-    setFilterValue(value);
-    onFilterChange(value);
+    if (!isNaN(value)) { // Check if the input is a number
+      setFilterValue(value);
+      onFilterChange(value);
+    }
   };
+
+  
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
@@ -26,8 +31,8 @@ const SidebarComponent = ({ onFilterChange, onSortChange }) => {
     <div>
       <span style={{ margin: '0 4% 0 0', color: '#fff'}}>SORT BY</span>
       <Select 
-        defaultValue="Type" 
-        onChange={onSortChange}
+        defaultValue="" 
+        onChange={onTypeChange}
         style={{ width: '50%' }}
       > 
         <Option value=""> All </Option>
@@ -51,20 +56,16 @@ const SidebarComponent = ({ onFilterChange, onSortChange }) => {
         <Option value="water">Water </Option>    
         </Select>
       </div>
-      <div style={{ display: 'flex' , alignItems: 'center', marginTop: '4%' }}>
+      <div style={{ display: 'flex' , justifyItems: 'center', marginTop: '8%' }}>
         <span style={{ margin: '0 4% 0 0', color: '#fff'}}>ID RANGE</span>
         <Input 
-          placeholder="ID" 
-          value={filterValue}
-          onChange={handleFilterChange}
-          style={{ width: '20%' }}
+          placeholder="ID Range Start" 
+          style={{ marginBottom: '20px' }}
         />
         <span style={{ margin: '0 4% 4% 4%', color: '#fff', fontSize: "40px"}}>-</span>
         <Input 
-          placeholder= "ID" 
-          value={filterValue}
-          onChange={handleFilterChange}
-          style={{ width: '20%' }}
+          placeholder="ID Range End" 
+          style={{ marginBottom: '20px' }}
         />
       </div>
     </Sider>
