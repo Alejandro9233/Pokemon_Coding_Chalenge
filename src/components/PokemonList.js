ac8dace13bd149791bf1529ec4f87bf3f5d63315
Alejandro9233 <a.barrera9233@gmail.com>
2024-10-28 16:06:01 -0600

    quick fix

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect, useMemo } from "react";
 import { fetchData } from "../utils/api";
-import { sortData, filterDataByName, filterDataByIndex } from "../utils/helpers";
+import {
+  sortData,
+  filterDataByName,
+  filterDataByIndex,
+} from "../utils/helpers";
 import Filter from "./Filter";
 import SidebarComponent from "./SideBar";
 import { List, Card } from "antd";
@@ -16,20 +20,20 @@ const PokemonList = () => {
   useEffect(() => {
     fetchData("/pokemon")
       .then((data) => {
-        const pokemonPromises = data.results.map(pokemon => {
-          return fetch(pokemon.url)
-            .then(response => response.json());
+        const pokemonPromises = data.results.map((pokemon) => {
+          return fetch(pokemon.url).then((response) => response.json());
         });
 
-        Promise.all(pokemonPromises)
-          .then(pokemonList => {
-            if (type !== "") {
-              const filteredPokemon = pokemonList.filter(p => p.types.some(t => t.type.name === type));
-              setPokemon(filteredPokemon);
-            } else {
-              setPokemon(pokemonList);
-            }
-          });
+        Promise.all(pokemonPromises).then((pokemonList) => {
+          if (type !== "") {
+            const filteredPokemon = pokemonList.filter((p) =>
+              p.types.some((t) => t.type.name === type)
+            );
+            setPokemon(filteredPokemon);
+          } else {
+            setPokemon(pokemonList);
+          }
+        });
       })
       .catch((error) => console.error(error));
   }, [type, location.pathname]);
@@ -50,29 +54,37 @@ const PokemonList = () => {
     setType(value);
   };
 
-  const filteredPokemonName = useMemo(() => filterDataByName(pokemon, filter), [
-    filter,
-    pokemon,
-  ]);
-  const sortedPokemon = useMemo(() => sortData(filteredPokemonName, sort), [
-    sort,
-    filteredPokemonName,
-  ]);
+  const filteredPokemonName = useMemo(
+    () => filterDataByName(pokemon, filter),
+    [filter, pokemon]
+  );
+  const sortedPokemon = useMemo(
+    () => sortData(filteredPokemonName, sort),
+    [sort, filteredPokemonName]
+  );
   const filteredPokemonID = useMemo(
     () => filterDataByIndex(sortedPokemon, filterID),
     [filterID, sortedPokemon]
   );
 
-console.log(filteredPokemonID);
-console.log(type);
+  console.log(filteredPokemonID);
+  console.log(type);
 
   return (
-    <div style={{ width: "1200px", margin: "0 auto", padding: "20px", display: "flex", background: "transparent"  }}>
-        <SidebarComponent
-          onFilterChange={handleFilterIDChange}
-          onTypeChange={handleTypeChange}
-        />
-      <div style={{ width: '100%' }}>
+    <div
+      style={{
+        width: "100%",
+        margin: "0 auto",
+        padding: "20px",
+        display: "flex",
+        background: "transparent",
+      }}
+    >
+      <SidebarComponent
+        onFilterChange={handleFilterIDChange}
+        onTypeChange={handleTypeChange}
+      />
+      <div style={{ width: "85%" }}>
         <Filter
           onFilterChange={handleFilterChange}
           onSortChange={handleSortChange}
@@ -90,14 +102,19 @@ console.log(type);
               <Link to={`/pokemon/${poke.name}`}>
                 <Card
                   hoverable
-                  style={{ borderRadius: "10px", transition: "all 0.3s ease", 
-                  background: "-webkit-linear-gradient(45deg, rgba(213, 15, 61, 0.6), rgba(13, 17, 198, 0.69) 100%)" }}
-                  cover={<img alt={poke.name} src={poke.sprites?.front_default} />}
+                  style={{
+                    borderRadius: "10px",
+                    transition: "all 0.3s ease",
+                    background:
+                      "-webkit-linear-gradient(45deg, rgba(213, 15, 61, 0.6), rgba(13, 17, 198, 0.69) 100%)",
+                  }}
+                  cover={
+                    <img alt={poke.name} src={poke.sprites?.front_default} />
+                  }
                   bodyStyle={{ display: "flex" }}
                 >
-                  <Card.Meta title={poke.name} style={{ color: '#fff'}} />
+                  <Card.Meta title={poke.name} style={{ color: "#fff" }} />
                 </Card>
-                
               </Link>
             </List.Item>
           )}
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,76 +1,84 @@
-import React, { useState } from 'react';
-import { Input, Select, Layout } from 'antd';
+import React, { useState } from "react";
+import { Input, Select, Layout } from "antd";
 
 const { Option } = Select;
 const { Sider } = Layout;
 
 const SidebarComponent = ({ onFilterChange, onTypeChange }) => {
-  const [filterValue, setFilterValue] = useState('');
+  const [filterValue, setFilterValue] = useState("");
 
-  
   const handleFilterChange = (event) => {
     const value = event.target.value;
-    if (!isNaN(value)) { // Check if the input is a number
+    if (!isNaN(value)) {
       setFilterValue(value);
       onFilterChange(value);
     }
   };
 
-  
-
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-    <Sider width={300} style={{ background: "#00aaff", padding: '20px', position: 'fixed', left: 10, borderRadius: "10px"}}>
-    <h2 style={{ color: '#fff' }} >Advance Search</h2>
-    <Input 
-      placeholder="Filter Pokémon by ID" 
-      value={filterValue}
-      onChange={handleFilterChange}
-      style={{ marginBottom: '20px' }}
-    />
-    <div>
-      <span style={{ margin: '0 4% 0 0', color: '#fff'}}>SORT BY</span>
-      <Select 
-        defaultValue="" 
-        onChange={onTypeChange}
-        style={{ width: '50%' }}
-      > 
-        <Option value=""> All </Option>
-        <Option value="bug"> Bug </Option>
-        <Option value="dragon">Dragon </Option>
-        <Option value="fairy">Fairy </Option>
-        <Option value="fire">Fire </Option>
-        <Option value="ghost">Ghost </Option>
-        <Option value="ground">Ground </Option>
-        <Option value="normal">Normal </Option>
-        <Option value="psychic">Psychic </Option>
-        <Option value="steel">Steel </Option>
-        <Option value="dark">Dark </Option>
-        <Option value="electric"></Option>
-        <Option value="fighting">Fighting </Option>
-        <Option value="flying">Flying </Option>
-        <Option value="grass">Grass </Option>
-        <Option value="ice">Ice </Option>
-        <Option value="poison">Poison </Option>
-        <Option value="rock">Rock </Option>
-        <Option value="water">Water </Option>    
-        </Select>
-      </div>
-      <div style={{ display: 'flex' , justifyItems: 'center', marginTop: '8%' }}>
-        <span style={{ margin: '0 4% 0 0', color: '#fff'}}>ID RANGE</span>
-        <Input 
-          placeholder="ID Range Start" 
-          style={{ marginBottom: '20px' }}
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider
+        width={300}
+        style={{
+          background: "#00aaff",
+          padding: "20px",
+          position: "fixed",
+          left: 10,
+          borderRadius: "10px",
+        }}
+      >
+        <h2 style={{ color: "#fff" }}>Advance Search</h2>
+        <Input
+          placeholder="Filter Pokémon by ID"
+          value={filterValue}
+          onChange={handleFilterChange}
+          style={{ marginBottom: "20px" }}
         />
-        <span style={{ margin: '0 4% 4% 4%', color: '#fff', fontSize: "40px"}}>-</span>
-        <Input 
-          placeholder="ID Range End" 
-          style={{ marginBottom: '20px' }}
-        />
-      </div>
-    </Sider>
-  </Layout>
-
+        <div>
+          <span style={{ margin: "0 4% 0 0", color: "#fff" }}>SORT BY</span>
+          <Select
+            defaultValue=""
+            onChange={onTypeChange}
+            style={{ width: "50%" }}
+          >
+            <Option value=""> All </Option>
+            <Option value="bug"> Bug </Option>
+            <Option value="dragon">Dragon </Option>
+            <Option value="fairy">Fairy </Option>
+            <Option value="fire">Fire </Option>
+            <Option value="ghost">Ghost </Option>
+            <Option value="ground">Ground </Option>
+            <Option value="normal">Normal </Option>
+            <Option value="psychic">Psychic </Option>
+            <Option value="steel">Steel </Option>
+            <Option value="dark">Dark </Option>
+            <Option value="electric"></Option>
+            <Option value="fighting">Fighting </Option>
+            <Option value="flying">Flying </Option>
+            <Option value="grass">Grass </Option>
+            <Option value="ice">Ice </Option>
+            <Option value="poison">Poison </Option>
+            <Option value="rock">Rock </Option>
+            <Option value="water">Water </Option>
+          </Select>
+        </div>
+        <div
+          style={{ display: "flex", justifyItems: "center", marginTop: "8%" }}
+        >
+          <span style={{ margin: "0 4% 0 0", color: "#fff" }}>ID RANGE</span>
+          <Input
+            placeholder="ID Range Start"
+            style={{ marginBottom: "20px" }}
+          />
+          <span
+            style={{ margin: "0 4% 4% 4%", color: "#fff", fontSize: "40px" }}
+          >
+            -
+          </span>
+          <Input placeholder="ID Range End" style={{ marginBottom: "20px" }} />
+        </div>
+      </Sider>
+    </Layout>
   );
 };
 
